dba0b9129af1dd70d6ac2e49d2ab0c79bb07399c
Daniel Pham <pham.dany@gmail.com>
2016-04-15 13:46:33 -0400

    Update benchmarks to for increased runtime

--- programs/benchmarks/primeNumbers.go
+++ programs/benchmarks/primeNumbers.go
@@ -4,51 +4,50 @@ package main
 // make it big to have a long computation time.
 
 func main() {
-  var iTHprimeNumber int = 10
+	var iTHprimeNumber int = 5000000
 
-  var primeNumbers []int
-  var p [3]int
-  primeNumbers = append(primeNumbers, 2)
+	var primeNumbers []int
+	primeNumbers = append(primeNumbers, 2)
 
-  var sqrt int = 2
-  var square int = 4
-  var counter int = 1
-  var number int = 2
-  var lastPrime int = 2
-  var i int = 0
-  var temp int = 0
-  var isPrime int = 1
-  var numPrimes int = 1
+	var sqrt int = 2
+	var square int = 4
+	var counter int = 1
+	var number int = 2
+	var lastPrime int = 2
+	var i int = 0
+	var temp int = 0
+	var isPrime int = 1
+	var numPrimes int = 1
 
-  for counter < iTHprimeNumber {
-    number++
+	for counter < iTHprimeNumber {
+		number++
 
-    if number > square {
-      sqrt++
-      square = sqrt*sqrt
-    }
+		if number > square {
+			sqrt++
+			square = sqrt * sqrt
+		}
 
-    isPrime = 1
-    for i = 0; i<numPrimes; i++ {
-      temp = primeNumbers[i]
+		isPrime = 1
+		for i = 0; i < numPrimes; i++ {
+			temp = primeNumbers[i]
 
-      if temp > sqrt {
-        break
-      }
+			if temp > sqrt {
+				break
+			}
 
-      if number % temp  == 0 {
-        isPrime = 0
-        break
-      }
-    }
+			if number%temp == 0 {
+				isPrime = 0
+				break
+			}
+		}
 
-    if isPrime == 1 {
-      primeNumbers = append(primeNumbers, number)
-      numPrimes++
-      lastPrime = number
-      counter++
-    }
-  }
+		if isPrime == 1 {
+			primeNumbers = append(primeNumbers, number)
+			numPrimes++
+			lastPrime = number
+			counter++
+		}
+	}
 
-  println(lastPrime)
+	println(lastPrime)
 }
--- programs/benchmarks/queenPuzzle.go
+++ programs/benchmarks/queenPuzzle.go
@@ -4,67 +4,66 @@ package main
 // place that many queens on the chessboard in a way that they wont be attacking eachother.
 
 type point struct {
-  x,y int
+	x, y int
 }
 
-var chessGridSize int = 8
+var chessGridSize int = 32
 var queens []point
-
 
 //returns 1 if everything is good
 func queenRecursion(index int) int {
-  if index >= chessGridSize {
-    return 1
-  }
+	if index >= chessGridSize {
+		return 1
+	}
 
-  var i int
-  var j int
-  var p point
-  var allgood int
-  var p2 point
-  var solutionFound int = 0
+	var i int
+	var j int
+	var p point
+	var allgood int
+	var p2 point
+	var solutionFound int = 0
 
-  p = queens[index]
+	p = queens[index]
 
-  for i = 0; i<chessGridSize; i++ {
-    p.x = i
+	for i = 0; i < chessGridSize; i++ {
+		p.x = i
 
-    allgood = 1
-    for j = 0; j<index; j++ {
-      p2 = queens[j]
+		allgood = 1
+		for j = 0; j < index; j++ {
+			p2 = queens[j]
 
-      if p.x == p2.x || p2.y - p.y == p2.x - p.x || p2.y - p.y == p.x - p2.x {
-        allgood = 0
-        break
-      }
-    }
+			if p.x == p2.x || p2.y-p.y == p2.x-p.x || p2.y-p.y == p.x-p2.x {
+				allgood = 0
+				break
+			}
+		}
 
-    if allgood == 1 {
-      queens[index] = p
-      if queenRecursion(index+1) == 1 {
-        print("(")
-        print(p.x)
-        print(",")
-        print(p.y)
-        println(")")
-        solutionFound = 1
-        break
-      }
-    }
-  }
+		if allgood == 1 {
+			queens[index] = p
+			if queenRecursion(index+1) == 1 {
+				print("(")
+				print(p.x)
+				print(",")
+				print(p.y)
+				println(")")
+				solutionFound = 1
+				break
+			}
+		}
+	}
 
-  return solutionFound
+	return solutionFound
 }
 
 func main() {
 
-  var i int
-  for i = 0; i<chessGridSize; i++ {
-    var p point
-    p.x = 0
-    p.y = i
-    queens = append(queens, p)
-  }
+	var i int
+	for i = 0; i < chessGridSize; i++ {
+		var p point
+		p.x = 0
+		p.y = i
+		queens = append(queens, p)
+	}
 
-  queenRecursion(0)
+	queenRecursion(0)
 }
